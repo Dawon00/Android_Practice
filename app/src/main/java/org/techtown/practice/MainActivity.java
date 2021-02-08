1ed83ddcfedf294448ab93beffcb766b926d441e
Dawon00 <da6921@naver.com>
2021-02-09 03:50:32 +0900

    button, image changing

--- app/src/main/java/org/techtown/practice/MainActivity.java
+++ app/src/main/java/org/techtown/practice/MainActivity.java
@@ -1,16 +1,44 @@
 package org.techtown.practice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    ImageView imageView2;
+
+    int imageIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//파라미터로 xml 파일을 넘겨줄 수 있다
+
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
     }
 
+    public void onButton1Clicked(View v){
+        changeImage();
+    }
 
+    private void changeImage()
+    {
+        if(imageIndex==0){
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+
+            imageIndex=1;
+        }
+        else if(imageIndex==1){
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+
+            imageIndex = 0;
+        }
+    }
 }
