338e8f0027fc50ae119b0f02eabc15e93619de03
Dawon00 <da6921@naver.com>
2021-02-13 23:40:40 +0900

    scrollView practice

--- app/src/main/java/org/techtown/practice/MainActivity.java
+++ app/src/main/java/org/techtown/practice/MainActivity.java
@@ -1,25 +1,37 @@
 package org.techtown.practice;
 
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ScrollView scrollView;
     ImageView imageView;
-    ImageView imageView2;
-
-    int imageIndex = 0;
+    BitmapDrawable bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//파라미터로 xml 파일을 넘겨줄 수 있다
 
+        scrollView = findViewById(R.id.scrollView);
         imageView = findViewById(R.id.imageView);
-        imageView2 = findViewById(R.id.imageView2);
+        scrollView.setHorizontalScrollBarEnabled(true);
+
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.img1);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
     }
 
     public void onButton1Clicked(View v){
@@ -28,17 +40,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void changeImage()
     {
-        if(imageIndex==0){
-            imageView.setVisibility(View.VISIBLE);
-            imageView2.setVisibility(View.INVISIBLE);
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.img2);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
 
-            imageIndex=1;
-        }
-        else if(imageIndex==1){
-            imageView.setVisibility(View.INVISIBLE);
-            imageView2.setVisibility(View.VISIBLE);
-
-            imageIndex = 0;
-        }
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
     }
 }
